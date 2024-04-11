6227c378f05a3f97678e11767e19f2d7bfe83a53
ApoTheddy <apotheddy@hotmail.com>
2024-04-11 14:56:35 -0500

    Update learning

---
+++ gallery/src/modules/gallery/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Index</title>
+ <link rel="stylesheet" href="../../assets/css/styles.css">
+</head>
+
+<body>
+
+ <header class="header">
+  <nav class="header__nav">
+   <ul class="header__ul">
+    <li class="header__li">Nav Link 1</li>
+    <li class="header__li">Nav Link 2</li>
+    <li class="header__li">Nav Link 3</li>
+    <li class="header__li">Nav Link 4</li>
+   </ul>
+  </nav>
+ </header>
+
+ <main class="main">
+  <!-- Top -->
+  <section class="main__section">
+   <ol class="main__ol">
+
+    <li class="main__li">
+     <img src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-346529.jpg&fm=jpg" alt="Image" class="main__img">
+     <p class="main__div--p">Shakuro</p>
+    </li>
+
+
+    <li class="main__li">
+     <img src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-346529.jpg&fm=jpg" alt="Image" class="main__img">
+     <p class="main__div--p">Shakuro</p>
+    </li>
+
+
+    <li class="main__li">
+     <img src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-346529.jpg&fm=jpg" alt="Image" class="main__img">
+     <p class="main__div--p">Shakuro</p>
+    </li>
+
+   </ol>
+  </section>
+
+ </main>
+</body>
+
+</html>
